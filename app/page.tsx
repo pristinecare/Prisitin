d8f2bcc992b9149129b84685586fc76d4f724d43
Prithalsing01 <prithal@egeniestores.in>
2026-06-19 23:26:16 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Reveal from "../components/Reveal";
+
 
 export default function Home() {
   const [activeTestimonial, setActiveTestimonial] = useState(0);
